7c6418ee6349318493dd755b3c6234abbffb109b
Borlay Dániel <borlay.daniel@gmail.com>
2015-11-01 11:51:41 +0100

    authentication implementation

--- frontend/html/js/login.js
+++
@@ -1,7 +0,0 @@
-angular.module('login', []).controller('loginController', function($scope) {
-	$scope.username = "";
-	$scope.password = "";
-    $scope.authorize = function (){
-    	window.location.href = "/portal.html";
-    }
-});
---
+++ frontend/html/modules/home/controllers.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('Home')
+
+.controller('HomeController',
+    ['$scope', '$rootScope', '$location', 'AuthenticationService',
+    function ($scope, $rootScope, $location, AuthenticationService) {
+
+    }]);
