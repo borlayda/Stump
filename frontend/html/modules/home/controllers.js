2fe33b56fb70d07dc3e36da17fa26536d16d7ea6
Borlay Dániel <borlay.daniel@gmail.com>
2015-11-11 01:30:54 +0100

    Dashboard parts integrated

--- frontend/html/modules/home/controllers.js
+++ frontend/html/modules/home/controllers.js
@@ -4,5 +4,11 @@ angular.module('Dashboard')
 .controller('DashboardController',
     ['$scope', '$rootScope', '$location',
     function ($scope, $rootScope, $location) {
+        $scope.dashboardPartActive = 'projects';
 
-}]);
+        $scope.switchActivePart = function($scope, partName) {
+            $scope.dashboardPartActive = patName;
+            console.log("Dashboard changed: Part = " + partName);
+        }
+    }
+]);
